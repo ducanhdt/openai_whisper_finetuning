5409f55d2bdae3657c0973ddefaf61f27f150be1
ducanh <ducanhdt>
2022-10-22 14:31:37 +0700

    add_train_translate_task

--- dataset.py
+++ dataset.py
@@ -167,6 +167,75 @@ class VivosTraining(torch.utils.data.Dataset):
             "text": text,
         }
 
+class VivosTrainingBothTask(torch.utils.data.Dataset):
+    def __init__(self, split="test", tokenizer=None, sample_rate=16000) -> None:
+        super().__init__()
+
+        root = f"data/vivos/{split}/waves"
+        dataset = []
+        with open(f"data/vivos/{split}/prompts.txt", "r") as f:
+            transcribe_text = f.read().split("\n")
+        with open(f"data/vivos/{split}/prompts_english.txt", "r") as f:
+            translate_texts = f.read().split("\n")
+        for text in transcribe_text:
+            x = text.find(" ")
+            audio_id, text = text[:x], text[x + 1 :]
+            speaker = audio_id.split("_")[0]
+            audio_path = f"{root}/{speaker}/{audio_id}.wav"
+            if os.path.isfile(audio_path):
+                dataset.append((audio_id, audio_path, text, "transcribe"))
+            
+        for text in translate_texts:
+            x = text.find(" ")
+            audio_id, text = text[:x], text[x + 1 :]
+            
+            speaker = audio_id.split("_")[0]
+            audio_path = f"{root}/{speaker}/{audio_id}.wav"
+            if os.path.isfile(audio_path):
+                dataset.append((audio_id, audio_path, text, "translate"))
+                    
+
+        self.dataset = dataset
+        # self.dataset = [self.dataset[i] for i in range(100)]
+        self.sample_rate = sample_rate
+        self.options = whisper.DecodingOptions(language="vi", without_timestamps=True)
+        self.translate_tokenizer = whisper.tokenizer.get_tokenizer(
+            True, language="vi", task="translate"
+        )
+        self.transcribe_tokenizer = whisper.tokenizer.get_tokenizer(
+            True, language="vi", task=self.options.task
+        )
+
+
+    def load_wave(self, wave_path, sample_rate: int = 16000) -> torch.Tensor:
+        waveform, sr = torchaudio.load(wave_path, normalize=True)
+        if sample_rate != sr:
+            waveform = at.Resample(sr, sample_rate)(waveform)
+        return waveform
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, id):
+        audio_id, audio_path, text, task = self.dataset[id]
+
+        audio = self.load_wave(audio_path, sample_rate=self.sample_rate)
+        audio = whisper.pad_or_trim(audio.flatten())
+        mel = whisper.log_mel_spectrogram(audio)
+        tokenizer = getattr(self,f"{task}_tokenizer")
+        text_token = [
+            *tokenizer.sot_sequence_including_notimestamps
+        ] + tokenizer.encode(text)
+        labels = text_token[1:] + [tokenizer.eot]
+        
+        return {
+            "input_ids": mel,
+            "labels": labels,
+            "dec_input_ids": text_token,
+            "text": text,
+        }
+
+
 
 if __name__ == "__main__":
     # dataset = LibriSpeech("test-clean")
